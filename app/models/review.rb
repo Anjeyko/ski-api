aa9c167058e6b72f8898eb05a0be59a3f6ba7407
Anjeyko <andriyberzerk@gmail.com>
2018-05-04 21:15:02 +0300

    Add validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :reviewable, polymorphic: true
+
+  validate :review_validation, on: :create
+
+  def review_validation
+    if reviewable_type == 'User'
+      unless Booking.joins(:item).where('items.user_id' => reviewable_id, 'bookings.user_id' => user_id).or(Booking.joins(:item).where('bookings.user_id' => reviewable_id, 'items.user_id' => user_id)).exists?
+        add_error(:review, "Error: you never dealt with this user")
+      end
+    elsif reviewable_type == 'Item'
+      unless user.bookings.where(item_id: reviewable_id).exists?
+        add_error(:review, "Error:you never booked this item")
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_01_183254) do
+ActiveRecord::Schema.define(version: 2018_05_02_102303) do
 
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2018_05_01_183254) do
     t.string "passport"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_users_on_city_id"
   end
 
 end
