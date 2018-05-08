0d0836db215cb558850ee50193de92a6fdb45aa1
Anjeyko <andriyberzerk@gmail.com>
2018-05-08 21:50:49 +0300

    Add CRUD

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,44 @@
 class ItemsController < ApplicationController
+  before_action :set_items, only: [:show, :edit, :update, :destroy]
+  
   def index
     items = Item.all
     paginate json: items.as_json, status: :ok
+  end
+
+  def show
+    render json: item.as_json, status: :ok
+  end
+
+  def create
+    firs_user = User.firs
+    item = user.items.new(item_params)
+    if item.save
+      render json: item.as_json, status: :created
+    else
+      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    if item.update(item_params)
+      head :no_content
+    else
+      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def destroy
+    @item.destroy
+    head :no_content
+  end
+
+  private
+  
+  def item_params
+    params.permit(:name, :user)
+  end
+
+  def set_items
+    @item = Item.find(params[:id])
   end
 end
